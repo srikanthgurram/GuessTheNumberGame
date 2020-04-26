8d483ae5238aec8875a696c580edf44c23de8ae3
srikanth gurram <2gurram@gmail.com>
2020-04-26 09:34:05 +0530

    Added icon and android configuration for building app

--- components/TitleText.js
+++ components/TitleText.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {Text, StyleSheet} from 'react-native'
+import LanguageStrings from '../constants/strings'
 
 const TitleText = props => {
+    const Strings = LanguageStrings[props.language];
     return (
         <Text style={{...styles.title, ...props.style}}>
             {props.children}
